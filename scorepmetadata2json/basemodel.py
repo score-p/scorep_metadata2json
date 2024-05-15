be96f950c8b293dd4362d29d9f5b84b653bcc7b1
Maximilian Sander <55374514+maximilian-tech@users.noreply.github.com>
2024-05-15 10:47:56 +0200

    Parse id and dates to schema (#5)
    Co-authored-by: Maximilian Sander <maximilian.sander1@tu-dresden.de>

--- scorepmetadata2json/basemodel.py
+++ scorepmetadata2json/basemodel.py
@@ -1,8 +1,11 @@
+from datetime import datetime
 from typing import List, Optional, Dict
 from pydantic import BaseModel, Field
 
 
 class LinkedType(BaseModel):
+    id: str = Field(..., description="The compile id.")
+    date: datetime = Field(..., description="The time of linking.")
     compiler: str = Field(..., description="The compiler name.")
     define_flags: Optional[List[str]] = Field(None, description="The define flags.")
     compiler_flags: Optional[List[str]] = Field(None, description="The compiler flags.")
@@ -14,6 +17,8 @@ class LinkedType(BaseModel):
 
 
 class Object(BaseModel):
+    id: str = Field(..., description="The compile id.")
+    date: datetime = Field(..., description="The time of compilation.")
     compiler: str = Field(..., description="The compiler name.")
     source_language: str = Field(..., description="The source language.")
 
@@ -37,6 +42,8 @@ class Instrumenter(BaseModel):
 
 
 class Runtime(BaseModel):
+    id: str = Field(..., description="The run id.")
+    date: datetime = Field(..., description="The time of execution.")
     env: Optional[Dict[str, Optional[str]]] = Field(
         None, description="The environment variables."
     )
